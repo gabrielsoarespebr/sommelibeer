e4a31fcbe6b7888f82a92c702ada8c177beda73c
Mauricio Valentim <62728083+mauriciovalentim@users.noreply.github.com>
2022-10-04 09:12:47 -0300

    Fluxo de rolagem beta
    Começo da criação do fluxo de rolagem de itens, ainda contém alguns bugs.

--- scripts/dinamics.js
+++ scripts/dinamics.js
@@ -1,4 +1,5 @@
-function arquivos(){
+function arquivos()
+{
     let drink01 = {name: "Brahma Duplo Malte 600ML", price: 6.99, rating: 4.0, comment: "", img: "images/drinks/brahmaduplomalte600ml.png"}
     let drink02 = {name: "Eisen Bahn Strong Golden Ale 355ML", price: 4.99, rating: 4.3, comment: "", img: "images/drinks/eisenbahnstronggoldenale355ml.png"}
     let drink03 = {name: "Eisenbahn Weizenbock 355ML", price: 6.90, rating: 3.9, comment: "", img: "images/drinks/eisenbahweizenbock355ml.png"}
@@ -8,18 +9,31 @@ function arquivos(){
     let valores = [drink01, drink02, drink03, drink04, drink05, drink06]   
     return valores 
 }
-let bebidas = arquivos()
-let a = document.querySelector(".products-container")
-for (var c=0; c<=3; c++){
-    console.log(bebidas[0].name)
-    a.innerHTML += '<div class="product" data-name="p-'+(c+1)+'">'+
-    '<img src='+bebidas[c].img+' alt="Produto 1">'+
-    '<h3>'+ bebidas[c].name+ '</h3>'+ 
-    '<span class="price">R$'+ bebidas[c].price+'</span>'+
-    '<span class="rating">'+ bebidas[c].rating+'</span>'+ 
-    '<fieldset class="comment">'+
-    '<legend>Avaliação em destaque</legend>'+
-    '<p>'+ bebidas[c].comment+'</p>'+
-    '</fieldset>'+
-    '</div>'
+function arrowFlow(andamento, fim)
+{
+    let bebidas = arquivos()
+    let pagina = document.querySelector(".products-container")
+    pagina.innerHTML = ""
+    for (var c=andamento; c<=fim; c++)
+    {
+        pagina.innerHTML += '<div class="product" data-name="p-'+(c+1)+'">'+
+        '<img src='+bebidas[c].img+' alt="Produto 1">'+
+        '<h3>'+ bebidas[c].name+ '</h3>'+ 
+        '<span class="price">R$'+ bebidas[c].price+'</span>'+
+        '<span class="rating">'+ bebidas[c].rating+'</span>'+ 
+        '<fieldset class="comment">'+
+        '<legend>Avaliação em destaque</legend>'+
+        '<p>'+ bebidas[c].comment+'</p>'+
+        '</fieldset>'+
+        '</div>'
+    }
 }
+function rodar(acrescimo)
+{
+    andamento =  acrescimo
+    fim +=  acrescimo
+    arrowFlow(andamento, fim)
+}
+
+var andamento =0, fim=3
+arrowFlow(andamento, fim)
