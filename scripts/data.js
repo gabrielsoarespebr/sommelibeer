fe764c2aa13693e27f3c9f56b9f6aede86df0654
Mauricio Valentim <62728083+mauriciovalentim@users.noreply.github.com>
2022-10-09 22:03:09 -0300

    updating database

---
+++ scripts/data.js
@@ -0,0 +1,11 @@
+export function arquivos() //Cria a lista de bebidas
+{
+    let drink01 = {name: "Brahma Duplo Malte 600ML", price: 6.99, rating: 4.0, comment: "", img: "images/drinks/brahmaduplomalte600ml.png"}
+    let drink02 = {name: "Eisen Bahn Strong Golden Ale 355ML", price: 4.99, rating: 4.3, comment: "", img: "images/drinks/eisenbahnstronggoldenale355ml.png"}
+    let drink03 = {name: "Eisenbahn Weizenbock 355ML", price: 6.90, rating: 3.9, comment: "", img: "images/drinks/eisenbahweizenbock355ml.png"}
+    let drink04 = {name: "Heineken 600ML", price: 9.60, rating: 4.6, comment: "", img: "images/drinks/heineken600ml.png"}
+    let drink05 = {name: "Hoegaarden 330ML", price: 5.25, rating: 4.4, comment: "", img: "images/drinks/hoegaarden330ml.png"}
+    let drink06 = {name: "Stella Artois Puro Malte Long Neck 330ML", price: 5.39, rating: 3.8, comment: "", img: "images/drinks/stellaartois330ml.png"}
+    let valores = [drink01, drink02, drink03, drink04, drink05, drink06]   
+    return valores 
+}
--- scripts/dinamics.js
+++ scripts/dinamics.js
@@ -1,14 +1,3 @@
-function arquivos() //Cria a lista de bebidas
-{
-    let drink01 = {name: "Brahma Duplo Malte 600ML", price: 6.99, rating: 4.0, comment: "", img: "images/drinks/brahmaduplomalte600ml.png"}
-    let drink02 = {name: "Eisen Bahn Strong Golden Ale 355ML", price: 4.99, rating: 4.3, comment: "", img: "images/drinks/eisenbahnstronggoldenale355ml.png"}
-    let drink03 = {name: "Eisenbahn Weizenbock 355ML", price: 6.90, rating: 3.9, comment: "", img: "images/drinks/eisenbahweizenbock355ml.png"}
-    let drink04 = {name: "Heineken 600ML", price: 9.60, rating: 4.6, comment: "", img: "images/drinks/heineken600ml.png"}
-    let drink05 = {name: "Hoegaarden 330ML", price: 5.25, rating: 4.4, comment: "", img: "images/drinks/hoegaarden330ml.png"}
-    let drink06 = {name: "Stella Artois Puro Malte Long Neck 330ML", price: 5.39, rating: 3.8, comment: "", img: "images/drinks/stellaartois330ml.png"}
-    let valores = [drink01, drink02, drink03, drink04, drink05, drink06]   
-    return valores 
-}
 function arrowAnimation(inicio, fim) //Deixa a seta cinza quando não rolar
 {
     if (inicio == 0){
@@ -42,7 +31,7 @@ function arrowFlow(inicio, fim) //Cria lista de bebidas
     }
     arrowAnimation(inicio, fim)
 }
-function rodar(acrescimo)
+export function rodar(acrescimo)
 {
     inicio +=  acrescimo
     fim +=  acrescimo
@@ -74,6 +63,8 @@ function rodar(acrescimo)
 }
 
 var setaEsquerda = document.querySelector(".seta");
+
+import {arquivos} from './data.js'
 var bebidas = arquivos()
 var inicio=0, fim=3
 arrowFlow(inicio, fim)
